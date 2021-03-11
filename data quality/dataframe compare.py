6817b16e365a27f570ff486e8b99633d845598bf
Michael T Hayes <19896726+michaelthayes@users.noreply.github.com>
2021-03-11 12:41:09 -0500

    split function into 2

--- data quality/dataframe compare.py
+++ data quality/dataframe compare.py
@@ -9,6 +9,58 @@ import pandas as pd
 
 
 
+
+
+
+# compares 2 dataframes with different columns, requires a mapper
+def set_index_mapper(source, target, mapper):
+
+    idx = mapper[mapper['idx'] == 'Y']
+
+    if not idx.empty:
+        for i, data in idx.iterrows():
+            if data['source'] not in source.columns:
+                raise ValueError('WARNING: source.columns is missing: ' + data['source'])
+            
+            if data['target'] not in target.columns:
+                raise ValueError('WARNING: target columns is missing: ' + data['target'])
+    
+        source = source.set_index(idx['source'].to_list())
+        target = target.set_index(idx['target'].to_list())
+    return(source, target)
+
+
+def compare_df_cols(source, target, mapper):
+    df = pd.DataFrame()
+
+    idx = mapper[mapper['idx'] == 'Y']
+
+    if not idx.empty:
+        for i, data in idx.iterrows():
+            if data['source'] not in source.columns:
+                raise ValueError('WARNING: source.columns is missing: ' + data['source'])
+            
+            if data['target'] not in target.columns:
+                raise ValueError('WARNING: target columns is missing: ' + data['target'])
+    
+        source = source.set_index(idx['source'].to_list())
+        target = target.set_index(idx['target'].to_list())
+    
+    for i, data in mapper[mapper['idx'] != 'Y'].iterrows():
+        if data['source'] not in source.columns:
+            raise ValueError('WARNING: source columns is missing: ' + data['source'])
+        
+        if data['target'] not in target.columns:
+            raise ValueError('WARNING: target columns is missing: ' + data['target'])
+        
+        df_tmp = source[data['source']].eq(target[data['target']])
+        df_tmp.name = data['source']
+        df = pd.concat([df, df_tmp], axis=1)
+        
+    return(df)
+
+
+
 df = pd.DataFrame({'cost': [250, 150, 100],
                    'revenue': [100, 250, 300],
                    'letters':['A', 'B', 'C']})
@@ -29,46 +81,16 @@ df2 = pd.DataFrame({'cost.new': [250, 150, 100],
 
 
 
-# compares 2 dataframes with different columns, requires a mapper
-def compare_df_cols(source, target, mapper):
-    df = pd.DataFrame()
-
-    idx = mapper[mapper['idx'] == 'Y']
-
-    if not idx.empty:
-        for i, data in idx.iterrows():
-            if data['source'] not in source.columns:
-                raise ValueError('WARNING: source.columns is missing: ' + data['source'])
-            
-            if data['target'] not in target.columns:
-                raise ValueError('WARNING: target columns is missing: ' + data['target'])
-    
-        source = source.set_index(idx['source'].to_list())
-        target = target.set_index(idx['target'].to_list())
-
-    
-    for i, data in mapper[mapper['idx'] != 'Y'].iterrows():
-        if data['source'] not in source.columns:
-            raise ValueError('WARNING: source columns is missing: ' + data['source'])
-        
-        if data['target'] not in target.columns:
-            raise ValueError('WARNING: target columns is missing: ' + data['target'])
-        
-        df_tmp = source[data['source']].eq(target[data['target']])
-        df_tmp.name = data['source']
-        df = pd.concat([df, df_tmp], axis=1)
-        
-    return(df)
-
-
-
-
 df_map = pd.read_csv('mapper_config.csv')
+
+df_idx, df2_idx = set_index_mapper(df, df2, df_map)
+
 result = compare_df_cols(df, df2, df_map)
 
 result
 
-df[result == False]
-df2[result == False]
+# are these returning the right responses?
+df_idx[result == False]
+df2_idx[result == False]
 
 
