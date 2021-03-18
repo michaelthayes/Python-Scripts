9b7adc4f2d254231fc16e732c5f59ff7530c436f
Michael T Hayes <19896726+michaelthayes@users.noreply.github.com>
2021-03-18 16:57:55 -0400

    updates, but not complete

--- reference/Various.py
+++ reference/Various.py
@@ -7,9 +7,14 @@ Created on Thu Nov  7 19:42:23 2019
 import pandas as pd
 
 
+
+def ConnectToDBServer(ServerName):
 #Create connection string to connect DBTest database with windows authentication
-#con = db.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER=' + Database + ';Trusted_Connection=yes')
-#cur = con.cursor()
+    return db.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER=' + ServerName + ';Trusted_Connection=yes')
+
+
+
+
 def readSQLScriptFromFile(scriptPath, con):
     with open(scriptPath, 'r') as inp:
         i=0
@@ -75,6 +80,44 @@ def ReadSQLServertoFileStore(file, schema, tableName, engine):
 #print(url_data)
 
 #response = requests.post(url_base, data=url_data, auth=(userID, pwdID), headers=headers)
+
+
+
+
+
+
+
+## not complete
+####  BELOW iterates through multiple results sets.
+def loop_through_results(cur, sqlQuery : str, df : pd.DataFrame):
+    i=0
+    #Fetch all rows using a while loop
+    while i < int(df.count()):
+        
+        
+        # loop through the results
+        cur.execute(sqlQuery + str(df.iloc[i,0]))
+        print(str(df.iloc[i,0]))
+    
+    
+        # Results set 1
+        Result1 = PullResults(cur)
+    
+        
+        cur.nextset ()
+        # Results set 2
+        cur.nextset ()
+        
+        #Results set 3
+        Result3 = PullResults(cur)
+        
+        # update the below to be an array
+        PolicyResults = PolicyResults.append(pd.DataFrame(Result1))
+        CoverageResults = CoverageResults.append(pd.DataFrame(Result3))
+        
+        i = i + 1
+
+    return df
 
 
 
